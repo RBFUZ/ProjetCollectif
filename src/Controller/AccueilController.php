8ff3a30c4bed26b57804d7bc6e2e2ce2dbd31cc1
Bouteloup remy <remy.bouteloup@yahoo.fr>
2018-03-12 16:21:07 +0100

    Ajout AProposEntreprise + modification route accueil

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -1,19 +1,20 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 
 class AccueilController extends Controller
 {
+	/**
+     * @Route("/accueil", name="accueil")
+     */
     public function affichage()
     {
-        
         	$user = "User / Admin";
-            return $this->render('accueil.html.twig', array(
+            return $this->render('accueil/index.html.twig', array(
 		'user' => $user,
 		));
-
-        
     }
 }
