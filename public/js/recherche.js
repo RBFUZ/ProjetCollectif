f0739302d6c98a8f7a0907105d95b72c0b6a2d34
Unknown <penghanyuan@163.com>
2018-04-04 17:32:30 +0200

    add comments

--- public/js/recherche.js
+++ public/js/recherche.js
@@ -1,22 +1,24 @@
-
+/**
+ * each page that you want to do some request(ajax) need a Vue
+ * Look at the doc of Vuejs
+ *
+ */
 
 var recherche_entreprise = new Vue({
-    el: '#recherche_entreprise',
+    el: '#recherche_entreprise', // the element that you want to control
     data: {
-        entreprises:[],
+        entreprises:[], // the data to the twig
         type:1
     },
     delimiters: ['${', '}'],
     methods:{
-        get_data: function (event) {
+        get_data: function (event) { // the methods that you want to excute
             var url = "/recherche_entreprise";
-
             //empty the array
             recherche_entreprise.entreprises.splice(0);
             $.post(url,{
                 nom_entreprise:$("input[name='nom_entreprise']").val(),
             },function(data){
-
                 // insert the data into array
                 for(i = 0;i<data.data.length;i++){
                     Vue.set(recherche_entreprise.entreprises, i,  data.data[i])
