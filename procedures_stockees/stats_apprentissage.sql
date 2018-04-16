4da7fe240c76c9828b32d1978b6610f3b8caf339
BenjaminPvr <pouvreau.benjamin@gmail.com>
2018-04-16 17:17:58 +0200

    Documentation procédures

--- procedures_stockees/stats_apprentissage.sql
+++ procedures_stockees/stats_apprentissage.sql
@@ -4,17 +4,27 @@ DROP PROCEDURE IF EXISTS display_apprenticeship_total_stats;
 DROP PROCEDURE IF EXISTS display_apprenticeship_stats_current_year;
 
 DELIMITER $$
+
+-- Displays the total number of apprenticeships in the database
 CREATE PROCEDURE display_apprenticeship_stats_total(
 	OUT po_total INT)
-
 BEGIN
 	SELECT COUNT(id) INTO po_total
 	FROM apprentissage;
 END$$
 
+
+
+-- Displays apprenticeship stats for the current year on a specified date
+-- INPUT
+-- 		pi_date [DATE] 		: current date, i.e. '2018-04-16'
+-- OUTPUTS
+-- 		po_total [INT] 		: number of apprenticeships related to the current year
+-- 		po_ongoing [INT]	: number of apprenticeships currently ongoing
+-- 		po_finished [INT]	: number of apprenticeships finished this year before pi_date
+-- 		po_tocome [INT]		: number of apprenticeships to come this year after pi_date
 CREATE PROCEDURE display_apprenticeship_stats_current_year(
 	IN pi_date DATE, OUT po_total INT, OUT po_ongoing INT, OUT po_finished INT, OUT po_tocome INT)
-
 BEGIN
 	-- Total for current year
 	SELECT COUNT(id) INTO po_total
