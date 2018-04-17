0c02ca8eacf31a65314561a389d5a40e3ed32bce
Bouteloup <remy.bouteloup@yahoo.fr>
2018-04-17 07:01:31 +0200

    Recherche entreprise (forum, taxe d'apprentissage) (visuel OK)

--- public/js/chart.js
+++ public/js/chart.js
@@ -56,6 +56,90 @@ var lineChartApprendata2 = {
         }
     ]
 };
+var lineChartTaxedataTotal = {
+    labels: [],
+    datasets: [
+        {
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }
+    ]
+};
+var lineChartTaxedataDI = {
+    labels: [],
+    datasets: [
+        {
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }
+    ]
+};
+var lineChartTaxedataDII = {
+    labels: [],
+    datasets: [
+        {
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }
+    ]
+};
+var lineChartTaxedataDAE = {
+    labels: [],
+    datasets: [
+        {
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }
+    ]
+};
+var lineChartTaxedataDMS = {
+    labels: [],
+    datasets: [
+        {
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }
+    ]
+};
+var lineChartTaxedataDEE = {
+    labels: [],
+    datasets: [
+        {
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
+        }
+    ]
+};
 
 function getYears(from) {
     var d1 = new Date(from),
@@ -102,6 +186,54 @@ $(document).ready(function () {
         maintainAspectRatio: true
     });
 
+    // taxe_apprentissage chartTotal
+    var taxe_apprentissage_cxtTotal = document.getElementById("line-area_taxeApprentissageTotal").getContext("2d");
+    lineChartTaxedataTotal.labels = getYears('2005');
+    taxe_apprentissageLineChartTotal = new Chart(taxe_apprentissage_cxtTotal).Line(lineChartTaxedataTotal, {
+        responsive: true,
+        maintainAspectRatio: true
+    });
+
+    // taxe_apprentissage chartDI
+    var taxe_apprentissage_cxtDI = document.getElementById("line-area_taxeApprentissageDI").getContext("2d");
+    lineChartTaxedataDI.labels = getYears('2005');
+    taxe_apprentissageLineChartDI = new Chart(taxe_apprentissage_cxtDI).Line(lineChartTaxedataDI, {
+        responsive: true,
+        maintainAspectRatio: true
+    });
+
+    // taxe_apprentissage chartDII
+    var taxe_apprentissage_cxtDII = document.getElementById("line-area_taxeApprentissageDII").getContext("2d");
+    lineChartTaxedataDII.labels = getYears('2005');
+    taxe_apprentissageLineChartDII = new Chart(taxe_apprentissage_cxtDII).Line(lineChartTaxedataDII, {
+        responsive: true,
+        maintainAspectRatio: true
+    });
+
+    // taxe_apprentissage chartDAE
+    var taxe_apprentissage_cxtDAE = document.getElementById("line-area_taxeApprentissageDAE").getContext("2d");
+    lineChartTaxedataDAE.labels = getYears('2005');
+    taxe_apprentissageLineChartDAE = new Chart(taxe_apprentissage_cxtDAE).Line(lineChartTaxedataDAE, {
+        responsive: true,
+        maintainAspectRatio: true
+    });
+
+    // taxe_apprentissage chartDMS
+    var taxe_apprentissage_cxtDMS = document.getElementById("line-area_taxeApprentissageDMS").getContext("2d");
+    lineChartTaxedataDMS.labels = getYears('2005');
+    taxe_apprentissageLineChartDMS = new Chart(taxe_apprentissage_cxtDMS).Line(lineChartTaxedataDMS, {
+        responsive: true,
+        maintainAspectRatio: true
+    });
+
+    // taxe_apprentissage chartDEE
+    var taxe_apprentissage_cxtDEE = document.getElementById("line-area_taxeApprentissageDEE").getContext("2d");
+    lineChartTaxedataDEE.labels = getYears('2005');
+    taxe_apprentissageLineChartDEE = new Chart(taxe_apprentissage_cxtDEE).Line(lineChartTaxedataDEE, {
+        responsive: true,
+        maintainAspectRatio: true
+    });
+
 
     $('.menu .item').click(function () {
         // stage chart1
@@ -129,6 +261,48 @@ $(document).ready(function () {
         // apprentissage chart2
         apprentissageLineChart2.destroy();
         apprentissageLineChart2 = new Chart(apprentissage_cxt2).Line(lineChartApprendata2, {
+            responsive: true,
+            maintainAspectRatio: true
+        });
+
+        // taxe_apprentissage chartTotal
+        taxe_apprentissageLineChartTotal.destroy();
+        taxe_apprentissageLineChartTotal = new Chart(taxe_apprentissage_cxtTotal).Line(lineChartTaxedataTotal, {
+            responsive: true,
+            maintainAspectRatio: true
+        });
+
+        // taxe_apprentissage chartDI
+        taxe_apprentissageLineChartDI.destroy();
+        taxe_apprentissageLineChartDI = new Chart(taxe_apprentissage_cxtDI).Line(lineChartTaxedataDI, {
+            responsive: true,
+            maintainAspectRatio: true
+        });
+
+        // taxe_apprentissage chartDII
+        taxe_apprentissageLineChartDII.destroy();
+        taxe_apprentissageLineChartDII = new Chart(taxe_apprentissage_cxtDII).Line(lineChartTaxedataDII, {
+            responsive: true,
+            maintainAspectRatio: true
+        });
+
+        // taxe_apprentissage chartDAE
+        taxe_apprentissageLineChartDAE.destroy();
+        taxe_apprentissageLineChartDAE = new Chart(taxe_apprentissage_cxtDAE).Line(lineChartTaxedataDAE, {
+            responsive: true,
+            maintainAspectRatio: true
+        });
+
+        // taxe_apprentissage chartDMS
+        taxe_apprentissageLineChartDMS.destroy();
+        taxe_apprentissageLineChartDMS = new Chart(taxe_apprentissage_cxtDMS).Line(lineChartTaxedataDMS, {
+            responsive: true,
+            maintainAspectRatio: true
+        });
+
+        // taxe_apprentissage chartDEE
+        taxe_apprentissageLineChartDEE.destroy();
+        taxe_apprentissageLineChartDEE = new Chart(taxe_apprentissage_cxtDEE).Line(lineChartTaxedataDEE, {
             responsive: true,
             maintainAspectRatio: true
         });
@@ -169,7 +343,6 @@ $(document).ready(function () {
             responsive: true,
             maintainAspectRatio: true
         });
-
     })
 });
 
--- public/js/establishmentDetail.js
+++ public/js/establishmentDetail.js
@@ -69,3 +69,41 @@ var dtable_apprenti = $("#data_table_apprenti").DataTable({
     retrieve: true,
 
 });
+
+var dtable_intervenant = $("#data_table_intervenant").DataTable({
+    language: {
+        "emptyTable": "No result"
+    },
+    // ajax:{
+    //     "url":"/search_enterprise",
+    //     "type":"post",
+    //     "data":
+    //         function ( d ) {
+    //             d.nom_enterprise = $("input[name='nom_enterprise']").val();
+    //         },
+    //     "complete": function(data) {
+    //         // $("#list_cities").empty();
+    //         // console.log(data["responseJSON"].data[0]);
+    //         // var d = data["responseJSON"].data;
+    //         // for(var i = 0;i<d.length;i++){
+    //         //     $("#list_cities").append('<div class="item"  data-value="'+d[i].idAdresse.idVille.id+'">'+d[i].idAdresse.idVille.nomVille+'</div>');
+    //         // }
+    //     }
+    // },
+    columns: [
+
+        {data: 'idContactEtablissement.idPersonne.nom'},
+        {data: 'idContactEtablissement.idPersonne.prenom'},
+        {data: "sujetConference"},
+        {data: "dateConference"},
+        {data: 'idContactEtablissement.mailProfessionnel'},
+    ],
+
+
+    filter: false,
+    info: false,
+    ordering: true,
+    processing: true,
+    retrieve: true,
+
+});
