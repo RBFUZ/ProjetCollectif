d352c10bb90c6c05a88650cfa4ea964bae5e4a09
Unknown <penghanyuan@163.com>
2018-04-17 14:08:14 +0200

    re-organize the code

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,61 +1,3 @@
-var lineChartInterndata1 = {
-    labels: [],
-    datasets: [
-        {
-            fillColor: "rgba(151,187,205,0.2)",
-            strokeColor: "rgba(151,187,205,1)",
-            pointColor: "rgba(151,187,205,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90]
-        }
-    ]
-};
-
-var lineChartInterndata2 = {
-    labels: [],
-    datasets: [
-        {
-            fillColor: "rgba(151,187,205,0.2)",
-            strokeColor: "rgba(151,187,205,1)",
-            pointColor: "rgba(151,187,205,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90]
-        }
-    ]
-};
-
-var lineChartApprendata1 = {
-    labels: [],
-    datasets: [
-        {
-            fillColor: "rgba(151,187,205,0.2)",
-            strokeColor: "rgba(151,187,205,1)",
-            pointColor: "rgba(151,187,205,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90]
-        }
-    ]
-};
-var lineChartApprendata2 = {
-    labels: [],
-    datasets: [
-        {
-            fillColor: "rgba(151,187,205,0.2)",
-            strokeColor: "rgba(151,187,205,1)",
-            pointColor: "rgba(151,187,205,1)",
-            pointStrokeColor: "#fff",
-            pointHighlightFill: "#fff",
-            pointHighlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90]
-        }
-    ]
-};
 
 function getYears(from) {
     var d1 = new Date(from),
@@ -69,112 +11,121 @@ function getYears(from) {
     return yr;
 }
 
-$(document).ready(function () {
-    // stage chart1
-    var cxt1 = document.getElementById("line-area1").getContext("2d");
-    lineChartInterndata1.labels = getYears('2005');
-    myLineChart1 = new Chart(cxt1).Line(lineChartInterndata1, {
+
+var makeLineChartData = function (label,data) {
+    var lineChartData = {
+        labels: label,
+        datasets: [
+            {
+                fillColor: "rgba(151,187,205,0.2)",
+                strokeColor: "rgba(151,187,205,1)",
+                pointColor: "rgba(151,187,205,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(151,187,205,1)",
+                data: data
+            }
+        ]
+    };
+    return lineChartData;
+}
+
+var makeLineChart = function (cxt,chartData) {
+    var cxt1 = document.getElementById(cxt).getContext("2d");
+    var lineChart = new Chart(cxt1).Line(chartData, {
         responsive: true,
         maintainAspectRatio: true
     });
+    return lineChart;
+}
 
-    // stage chart2
-    var cxt2 = document.getElementById("line-area2").getContext("2d");
-    lineChartInterndata2.labels = getYears('2005');
-    myLineChart2 = new Chart(cxt2).Line(lineChartInterndata2, {
-        responsive: true,
-        maintainAspectRatio: true
-    });
-
-    //apprentissage chart1
-    var apprentissage_cxt1 = document.getElementById("line-area-apprentissage1").getContext("2d");
-    lineChartApprendata1.labels = getYears('2005');
-    apprentissageLineChart1 = new Chart(apprentissage_cxt1).Line(lineChartApprendata1, {
-        responsive: true,
-        maintainAspectRatio: true
-    });
-
-    //apprentissage chart2
-    var apprentissage_cxt2 = document.getElementById("line-area-apprentissage2").getContext("2d");
-    lineChartApprendata2.labels = getYears('2005');
-    apprentissageLineChart2 = new Chart(apprentissage_cxt2).Line(lineChartApprendata2, {
-        responsive: true,
-        maintainAspectRatio: true
-    });
-
-
-    $('.menu .item').click(function () {
-        // stage chart1
-        myLineChart1.destroy();
-        myLineChart1 = new Chart(cxt1).Line(lineChartInterndata1, {
-            responsive: true,
-            maintainAspectRatio: true
-        });
-
-
-        // stage chart2
-        myLineChart2.destroy();
-        myLineChart2 = new Chart(cxt2).Line(lineChartInterndata2, {
-            responsive: true,
-            maintainAspectRatio: true
-        });
-
-        // apprentissage chart1
-        apprentissageLineChart1.destroy();
-        apprentissageLineChart1 = new Chart(apprentissage_cxt1).Line(lineChartApprendata1, {
-            responsive: true,
-            maintainAspectRatio: true
-        });
-
-        // apprentissage chart2
-        apprentissageLineChart2.destroy();
-        apprentissageLineChart2 = new Chart(apprentissage_cxt2).Line(lineChartApprendata2, {
-            responsive: true,
-            maintainAspectRatio: true
-        });
-    });
-
-    $("input[name='department']").change(function () {
-        myLineChart1.destroy();
-        // change data
-        lineChartInterndata1.datasets[0].data = getDataForCharts($(this).val())
-        myLineChart1 = new Chart(cxt1).Line(lineChartInterndata1, {
-            responsive: true,
-            maintainAspectRatio: true
-        });
-
-        // stage chart2
-        myLineChart2.destroy();
-        // change data
-        lineChartInterndata2.datasets[0].data = getDataForCharts($(this).val())
-        myLineChart2 = new Chart(cxt2).Line(lineChartInterndata2, {
-            responsive: true,
-            maintainAspectRatio: true
-        });
-
-        // apprentissage chart1
-        apprentissageLineChart1.destroy();
-        // change data
-        lineChartApprendata1.datasets[0].data = getDataForCharts($(this).val())
-        apprentissageLineChart1 = new Chart(apprentissage_cxt1).Line(lineChartApprendata1, {
-            responsive: true,
-            maintainAspectRatio: true
-        });
-
-        // apprentissage chart2
-        apprentissageLineChart2.destroy();
-        // change data
-        lineChartApprendata2.datasets[0].data = getDataForCharts($(this).val())
-        apprentissageLineChart2 = new Chart(apprentissage_cxt2).Line(lineChartApprendata2, {
-            responsive: true,
-            maintainAspectRatio: true
-        });
-
-    })
-});
-
+// update data from database filter by department
 var getDataForCharts = function (department) {
     // get data from database
     console.log(department)
     return [1,2,3,4,5,6];
 }
+
+$(document).ready(function () {
+    // stage chart1
+    lineChartInterndata1 = makeLineChartData(getYears('2005'),[1,2,3,4,52]);
+    myLineChart1 = makeLineChart("line-area1",lineChartInterndata1);
+
+    // stage chart2
+    lineChartInterndata2 = makeLineChartData(getYears('2005'),[23,2,12,3]);
+    myLineChart2 = makeLineChart("line-area2",lineChartInterndata2);
+
+    //apprentissage chart1
+    lineChartApprendata1 = makeLineChartData(getYears('2005'),[23,2,12,3]);
+    apprentissageLineChart1 = makeLineChart("line-area-apprentissage1",lineChartApprendata1);
+
+    //apprentissage chart2
+    lineChartApprendata2  = makeLineChartData(getYears('2005'),[23,2,12,3]);
+    apprentissageLineChart2 = makeLineChart("line-area-apprentissage2",lineChartApprendata2);
+
+   
+    // disable department for forum and conference
+    $("#tab_etab").ready(function () {
+        if($("#tab_etab .active").attr("data-tab")==="fourth"||$("#tab_etab .active").attr("data-tab")==="third"){
+            $("#department_select").hide();
+        }
+        else{
+            $("#department_select").show();
+        }
+    })
+
+    // tab clicked
+    $('.menu .item').click(function () {
+        // stage chart1
+        myLineChart1.destroy();
+        myLineChart1 = makeLineChart("line-area1",lineChartInterndata1);
+
+
+        // stage chart2
+        myLineChart2.destroy();
+        myLineChart2 = makeLineChart("line-area2",lineChartInterndata2);
+
+        // apprentissage chart1
+        apprentissageLineChart1.destroy();
+        apprentissageLineChart1 = makeLineChart("line-area-apprentissage1",lineChartApprendata1);
+
+        // apprentissage chart2
+        apprentissageLineChart2.destroy();
+        apprentissageLineChart2 = makeLineChart("line-area-apprentissage2",lineChartApprendata2);
+
+        console.log($(this).text())
+        // disable the department select for forum and conference
+        if($(this).text()==="Forum" || $(this).text()==="Conférence") {
+            $("#department_select").hide();
+        }
+        else{
+            $("#department_select").show();
+        }
+    });
+
+    // department change
+    $("input[name='department']").change(function () {
+        myLineChart1.destroy();
+        // change data
+        lineChartInterndata1.datasets[0].data = getDataForCharts($(this).val())
+        myLineChart1 = makeLineChart("line-area1",lineChartInterndata1);
+        // stage chart2
+        myLineChart2.destroy();
+        // change data
+        lineChartInterndata2.datasets[0].data = getDataForCharts($(this).val())
+        myLineChart2 = makeLineChart("line-area2",lineChartInterndata2);
+
+        // apprentissage chart1
+        apprentissageLineChart1.destroy();
+        // change data
+        lineChartApprendata1.datasets[0].data = getDataForCharts($(this).val())
+        apprentissageLineChart1 = makeLineChart("line-area-apprentissage1",lineChartApprendata1);
+
+        // apprentissage chart2
+        apprentissageLineChart2.destroy();
+        // change data
+        lineChartApprendata2.datasets[0].data = getDataForCharts($(this).val())
+        apprentissageLineChart2 = makeLineChart("line-area-apprentissage2",lineChartApprendata2);
+    })
+});
+
