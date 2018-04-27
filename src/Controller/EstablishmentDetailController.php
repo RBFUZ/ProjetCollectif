f9a3e4146ff4be3436eabf21eb0755ff907406ac
RBFUZ <remy.bouteloup@yahoo.fr>
2018-04-27 12:03:01 +0200

    RechercheEntreprise - Forum - Année OK - Changer icon vert/rouge à faire

--- src/Controller/EstablishmentDetailController.php
+++ src/Controller/EstablishmentDetailController.php
@@ -4,10 +4,13 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Validator\Constraints\DateTime;
 use App\Entity\Etablissement;
 use App\Entity\ConventionStage;
 use App\Entity\Apprentissage;
 use App\Entity\Conference;
+use App\Entity\TypeForum;
+use App\Entity\Forum;
 
 class EstablishmentDetailController extends Controller
 {
@@ -28,11 +31,54 @@ class EstablishmentDetailController extends Controller
         $repository_convention = $this->getDoctrine()->getRepository(Conference::class);
         $convention = $repository_convention->findConventionByEstablishment($idEstablishment);
 
+        $repository_type_forum = $this->getDoctrine()->getRepository(TypeForum::class);
+        $type_forum = $repository_type_forum->findAll();
+
+        $type_forum = $this->checkIfForumCreateOrNot($type_forum); // // Supprime les types de forum du tableau quand aucun forum de ce type n'a été ajouté
+        $years_forum = $this->fillYearForumUntilToday($type_forum); // Remplir les années de la plus ancienne jusqu'a aujourd'hui
+
         return $this->render('establishment_detail/index.html.twig', [
             'establishment' => $establishment,
             'trainee' => $trainee,
             'apprenticeship' => $apprenticeship,
-            'convention' => $convention
+            'convention' => $convention,
+            'type_forum' => $type_forum,
+            'forum' => $years_forum
         ]);
+    }
+
+    public function checkIfForumCreateOrNot($type_forum): array
+    {
+        foreach ($type_forum as $key=>$type) {
+            $repository_forum = $this->getDoctrine()->getRepository(Forum::class); // Récupération de l'année la plus ancienne (forum)
+            $forum = $repository_forum->checkIfForumExist($type->getId());
+
+            if (count($forum) == 0) {
+                unset($type_forum[$key]); // Supprime les types de forum du tableau quand aucun forum de ce type n'a été ajouté.
+            }
+        }
+        return $type_forum;
+    }
+
+    public function fillYearForumUntilToday($type_forum): array
+    {
+        $years_array_all_forum = array(); // Contiendra l'ensemble des années pour chacun des types de forum (tableau deux dimensions)
+
+        foreach ($type_forum as $key=>$type) {
+            $repository_forum = $this->getDoctrine()->getRepository(Forum::class); // Récupération de l'année la plus ancienne (forum)
+            $forum = $repository_forum->getOldestForum($type->getId());
+
+            $oldest_year = date_format($forum[0]['dateDebutForum'], "Y"); // Extraire l'année de la date reçu
+            $current_year = date("Y"); // Année courante
+
+            $year_array_one_forum = array();
+
+            for ($i = $oldest_year; $i <= $current_year; $i++) {
+                array_push($year_array_one_forum, $i);
+            }
+
+            array_push($years_array_all_forum, $year_array_one_forum);
+        }
+        return $years_array_all_forum;
     }
 }
