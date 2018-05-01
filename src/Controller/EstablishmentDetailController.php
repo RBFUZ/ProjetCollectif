a0c2480ac7d6e570734f730045e2a67800c56300
Bouteloup <remy.bouteloup@yahoo.fr>
2018-05-01 13:09:05 +0200

    Update design modifications demandées par Patrick

--- src/Controller/EstablishmentDetailController.php
+++ src/Controller/EstablishmentDetailController.php
@@ -178,7 +178,7 @@ class EstablishmentDetailController extends Controller
         foreach ($type_forum as $key=>$type) {
             $forum = $repository_forum->getOldestForum($type->getLibelleTypeForum()); // Récupération de l'année la plus ancienne (forum)
 
-            $oldest_year = date_format($forum[0]['dateDebutForum'], "Y"); // Extraire l'année de la date reçu
+            $oldest_year = strtok($forum[0]["date"], '-'); // Extraire l'année de la date reçu
             $current_year = date("Y"); // Année courante
 
             $year_array_one_forum = array();
