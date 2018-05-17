4495d20fb43969a9ec344c9d274b9ba247c606dc
unknown <mohamed96amine@gmail.com>
2018-05-17 11:53:09 +0200

    authentification

---
+++ src/Controller/RegisterController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use App\Entity\User;
+use App\Form\RegisterType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+
+class RegisterController extends Controller
+{
+    /**
+     * @Route("/register", name="register")
+     */
+    public function registerAction(Request $request, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $password = $passwordEncoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+
+            $user->setRoles(['ROLE_ADMIN']);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('index');
+        }
+
+        return $this->render(
+            'register/index.html.twig',
+            array('form' => $form->createView())
+        );
+    }
+
+}
